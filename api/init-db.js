a232c9fdb21c10599ed42ef47c1f6cee47981284
hrhdegenetrix <hrhdegenetrix@users.noreply.github.com>
2025-06-16 22:02:06 -0700

    Allow GET requests for database initialization

--- api/init-db.js
+++ api/init-db.js
@@ -6,7 +6,7 @@ module.exports = async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
@@ -14,7 +14,7 @@ module.exports = async function handler(req, res) {
     return;
   }
 
-  if (req.method !== 'POST') {
+  if (req.method !== 'GET' && req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
